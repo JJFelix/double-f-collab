fd7e5dc2284169308c3d53e5b82221945638707c
Mandillah123* <official1337h4x0r@gmail.com>
2023-06-06 16:11:46 +0530

    main.jsx restructure

--- FRONTEND/dow/src/main.jsx
+++ FRONTEND/dow/src/main.jsx
@@ -5,7 +5,8 @@ import Navbar from './Navbar.jsx'
 import Body from './Body.jsx'
 import Footer from './Footer.jsx'
 import './index.css'
-import 'font-awesome/css/font-awesome.min.css'             
+import 'font-awesome/css/font-awesome.min.css'   
+          
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
